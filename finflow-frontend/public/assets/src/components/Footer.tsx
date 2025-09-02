e3cceb924b2d0a7cbebbb7a45d7950fc65b9af8b
Kingspath <162287298+Kingspath@users.noreply.github.com>
2025-09-02 23:08:43 +0200

    Delete finflow-frontend directory

--- finflow-frontend/public/assets/src/components/Footer.tsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="bg-finflow-dark text-white text-center py-4">
-      <p>© {new Date().getFullYear()} FinFlow. All rights reserved.</p>
-    </footer>
-  );
-}
