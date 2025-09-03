4b6261b92ccf24ef8a18e35053a5cc1843ab38b9
Kingspath <162287298+Kingspath@users.noreply.github.com>
2025-09-03 06:30:07 +0000

    Sentry configuration

---
+++ sentry.server.config.js
@@ -0,0 +1,18 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://3774b46b9d176890de941ec4f0e94bc4@o4509146460258304.ingest.de.sentry.io/4509953863647312",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
